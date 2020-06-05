87aa1256fcf740573843f83188e3a80bdcbb2b48
Default Profile <znatokly@gmail.com>
2020-06-05 16:02:22 +0300

    Updates

---
+++ precache-manifest.ff6ba42e9d78ff251309fecce7d0eda2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3040465bb14e609c8cdb197330c7e870",
+    "url": "/telephonebook/index.html"
+  },
+  {
+    "revision": "24eb1d80307f85443926",
+    "url": "/telephonebook/static/js/2.cf89add6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/telephonebook/static/js/2.cf89add6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a216370972ee9922250d",
+    "url": "/telephonebook/static/js/main.ac96726c.chunk.js"
+  },
+  {
+    "revision": "5ec354fa22ada8cda6da",
+    "url": "/telephonebook/static/js/runtime-main.2cae8237.js"
+  }
+]);
